2085577e1e7ba48d9ee4d9e79e87d016d6349fa2
kvarel <a.parshin@comearth.ru>
2023-02-20 15:04:14 +0300

    -

--- call.js
+++ call.js
@@ -1,201 +1,252 @@
-function loadPage() {
-    console.log('on loadPage');
+$(document).ready(() => initial())
 
-    $("#loginText").val(localStorage.getItem("login"));
-    $("#passwordText").val(localStorage.getItem("pwd"));
-    $("#callNumberText").val(localStorage.getItem("callNumber"));
+const SERVER = '10.88.0.70'
+const PORT = '8089'
+const loginAlias = 'login'
+const passwordAlias = 'pwd'
+const callNumberAlias = 'callNumber'
+let soundsControl
+let remoteAudioControl
+let loginInput
+let passwordInput
+let loginButton
+let callNumberInput
+let answerButton
+let logOutButton
+let callPanel
+let callButton
+let hangUpButton
+let socket
+let ua
+let session
+let stream
 
-    this._soundsControl = document.getElementById("sounds");
+
+function initial() {
+    // заполнение справочника элементов
+    soundsControl = document.querySelector("#sounds")
+    remoteAudioControl = document.querySelector("#remoteAudio")
+    loginInput = $("#loginText")
+    passwordInput = $("#passwordText")
+    loginButton = $("#loginButton")
+    callNumberInput = $("#callNumberText")
+    logOutButton = $("#logOutButton")
+    callPanel = $("#callPanel")
+    callButton = $('#callNumberButton')
+    answerButton = $('#answerButton')
+    hangUpButton = $('#hangUpButton')
+
+    // если в локальном хранилище есть данные прошлых сессий, производим автозаполнение
+    loginInput.val(localStorage.getItem(loginAlias))
+    passwordInput.val(localStorage.getItem(passwordAlias))
+    callNumberInput.val(localStorage.getItem(callNumberAlias))
+
+    // добавляем события на кнопки
+    loginButton.click(login)
+    logOutButton.click(logout)
+    callButton.click(call)
+    hangUpButton.click(hangUp)
+    answerButton.click(answer)
+
+    // подготовка отображения к работе
+    logoutSuccess()
 }
+
 
 function login() {
-    console.log("on login");
-    this.loginText = $("#loginText");
-    this.passwordText = $("#passwordText");
-    this.loginButton = $("#loginButton");
-    this.logOutButton = $("#logOutButton");
-    this.callButton = $('#callNumberButton');
-    this.hangUpButton = $('#hangUpButton');
+    localStorage.setItem(loginAlias, loginInput.val())
+    localStorage.setItem(passwordAlias, passwordInput.val())
 
-
-    localStorage.setItem("login", this.loginText.val());
-    localStorage.setItem("pwd", this.passwordText.val());
-
-    socket = new JsSIP.WebSocketInterface("wss://YOUR_SERVER:PORT/ws");
-    _ua = new JsSIP.UA(
+    socket = new JsSIP.WebSocketInterface(`wss://${SERVER}:${PORT}/ws`)
+    ua = new JsSIP.UA(
         {
-            uri: "sip:" + this.loginText.val() + "@YOUR_SERVER",
-            password: this.passwordText.val(),
-            display_name: this.loginText.val(),
+            uri: `sip:${loginInput.val()}@${SERVER}`,
+            password: passwordInput.val(),
+            display_name: loginInput.val(),
             sockets: [socket]
-        });
+        })
+    ua.start()
 
-    // соединяемся с астером
-    this._ua.on('connecting', () => {
-        console.log("UA connecting");
-    });
-
-    // соединились с астером
-    this._ua.on('connected', () => {
-        console.log("UA connected");
-    });
-
-    // астер нас зарегал, теперь можно звонить и принимать звонки
-    this._ua.on('registered', () => {
-        console.log("UA registered");
-
-        this.loginButton.addClass('d-none');
-        this.logOutButton.removeClass('d-none');
-        this.loginText.prop('disabled', true);
-        this.passwordText.prop('disabled', true);
-
-        $("#callPanel").removeClass('d-none');
-    });
-
-    // астер про нас больше не знает
-    this._ua.on('unregistered', () => {
-        console.log("UA unregistered");
-    });
-
-    // астер не зарегал нас, что то не то, скорее всего неверный логин или пароль
-    this._ua.on('registrationFailed', (data) => {
-        console.error("UA registrationFailed", data.cause);
-    });
-
-    // заводим шарманку
-    this._ua.start();
+    ua.on('registered', loginSuccess)
+    ua.on('unregistered', logoutSuccess)
+    ua.on('registrationFailed', loginFail)
+    ua.on('newRTCSession', dispatchCall)
 }
+
 
 function logout() {
-    console.log("on logout")
-
-    this.loginButton.removeClass('d-none');
-    this.logOutButton.addClass('d-none');
-    this.loginText.prop('disabled', false);
-    this.passwordText.prop('disabled', false);
-
-    $("#callPanel").addClass('d-none');
-
-    // закрываем всё нафиг, вылогиниваемся из астера, закрываем коннект
-    this._ua.stop();
+    ua.stop()
 }
+
+
+function loginSuccess() {
+    loginButton.hide()
+    logOutButton.show()
+    loginInput.attr('disabled', true)
+    passwordInput.attr('disabled', true)
+    callPanel.show()
+    answerButton.hide()
+    hangUpButton.hide()
+}
+
+
+function loginFail(data) {
+    console.error("UA registrationFailed", data.cause)
+}
+
+
+function logoutSuccess() {
+    loginButton.show()
+    logOutButton.hide()
+    loginInput.attr('disabled', false)
+    passwordInput.attr('disabled', false)
+    callPanel.hide()
+}
+
+
+function callStart() {
+    callButton.hide()
+    hangUpButton.show()
+}
+
+
+function callEnd() {
+    hangUpButton.hide()
+    callButton.show()
+    answerButton.hide()
+
+}
+
+
+function incomingCall() {
+    callButton.hide()
+    answerButton.show()
+    hangUpButton.show()
+}
+
 
 function call() {
-    let number = $('#callNumberText').val();
-    localStorage.setItem("callNumber", number);
-
-    this.callButton.addClass('d-none');
-    this.hangUpButton.removeClass('d-none');
-
-    // Делаем ИСХОДЯЩИЙ звонок
-    // Принимать звонки этот код не умеет!
-    this.session = this._ua.call(number, {
+    ua.call(callNumberInput.val(), {
         pcConfig:
-        {
-            hackStripTcp: true, // Важно для хрома, чтоб он не тупил при звонке
-            rtcpMuxPolicy: 'negotiate', // Важно для хрома, чтоб работал multiplexing. Эту штуку обязательно нужно включить на астере.
-            iceServers: []
-        },
+            {
+                hackStripTcp: true, // Важно для хрома, чтоб он не тупил при звонке
+                rtcpMuxPolicy: 'negotiate', // Важно для хрома, чтоб работал multiplexing. Эту штуку обязательно нужно включить на астере.
+                iceServers: []
+            },
         mediaConstraints:
-        {
-            audio: true, // Поддерживаем только аудио
-            video: false
-        },
+            {
+                audio: true, // Поддерживаем только аудио
+                video: false
+            },
         rtcOfferConstraints:
-        {
-            offerToReceiveAudio: 1, // Принимаем только аудио
-            offerToReceiveVideo: 0
-        }
-    });
-
-    // Это нужно для входящего звонка, пока не используем
-    this._ua.on('newRTCSession', (data) => {
-        if (!this._mounted)
-            return;
-
-        if (data.originator === 'local')
-            return;
-
-        // audioPlayer.play('ringing');
-    });
-
-    // Астер нас соединил с абонентом
-    this.session.on('connecting', () => {
-        console.log("UA session connecting");
-        playSound("ringback.ogg", true);
-
-        // Тут мы подключаемся к микрофону и цепляем к нему поток, который пойдёт в астер
-        let peerconnection = this.session.connection;
-        let localStream = peerconnection.getLocalStreams()[0];
-
-        // Handle local stream
-        if (localStream) {
-            // Clone local stream
-            this._localClonedStream = localStream.clone();
-
-            console.log('UA set local stream');
-
-            let localAudioControl = document.getElementById("localAudio");
-            localAudioControl.srcObject = this._localClonedStream;
-        }
-
-        // Как только астер отдаст нам поток абонента, мы его засунем к себе в наушники
-        peerconnection.addEventListener('addstream', (event) => {
-            console.log("UA session addstream");
-
-            let remoteAudioControl = document.getElementById("remoteAudio");
-            remoteAudioControl.srcObject = event.stream;
-        });
-    });
-
-    // В процессе дозвона
-    this.session.on('progress', () => {
-        console.log("UA session progress");
-        playSound("ringback.ogg", true);
-    });
-
-    // Дозвон завершился неудачно, например, абонент сбросил звонок
-    this.session.on('failed', (data) => {
-        console.log("UA session failed");
-        stopSound("ringback.ogg");
-        playSound("rejected.mp3", false);
-
-        this.callButton.removeClass('d-none');
-        this.hangUpButton.addClass('d-none');
-    });
-
-    // Поговорили, разбежались
-    this.session.on('ended', () => {
-        console.log("UA session ended");
-        playSound("rejected.mp3", false);
-        JsSIP.Utils.closeMediaStream(this._localClonedStream);
-
-        this.callButton.removeClass('d-none');
-        this.hangUpButton.addClass('d-none');
-    });
-
-    // Звонок принят, моно начинать говорить
-    this.session.on('accepted', () => {
-        console.log("UA session accepted");
-        stopSound("ringback.ogg");
-        playSound("answered.mp3", false);
-    });
+            {
+                offerToReceiveAudio: 1, // Принимаем только аудио
+                offerToReceiveVideo: 0
+            }
+    })
 }
+
+
+function dispatchCall(data) {
+    callStart()
+    localStorage.setItem(callNumberAlias, callNumberInput.val())
+    session = data.session
+    switch (data.originator) {
+        case 'remote':
+            incomeCall(data)
+            break
+        case 'local':
+            outgoingCall(data)
+            break
+    }
+}
+
+
+function incomeCall(data) {
+    session.on('progress', () => {
+        playSound("ringing.ogg", true)
+        incomingCall()
+    })
+    session.on('connecting', () => {
+        let reseivers = session.connection.getReceivers()
+        stream = new MediaStream()
+        reseivers.forEach(r => stream.addTrack(r.track))
+        remoteAudioControl.srcObject = stream
+        remoteAudioControl.play()
+    })
+    session.on('accepted', () => {
+        stopSound("ringing.ogg")
+        answerButton.hide()
+    })
+    session.on('failed', () => {
+        stopSound("ringing.ogg")
+        playSound("rejected.mp3", false)
+        callEnd()
+    })
+    session.on('ended', callEnd)
+}
+
+
+function answer() {
+    session.answer({
+        mediaConstraints: {
+            audio: true,
+            video: false
+        }
+    })
+}
+
+
+function outgoingCall(data) {
+    // // сразу
+    // session.on('connecting', () => {console.log('---------------connecting-------------------')})
+    // // звонок пошел
+    // session.on('progress', () => {console.log('---------------progress-------------------')})
+    // // после того, как принял звонок
+    // session.on('accepted', () => {console.log('---------------accepted-------------------')})
+    // session.on('confirmed', () => {console.log('---------------confirmed-------------------')})
+    // // после того, как нажал завершить
+    // session.on('ended', () => {console.log('---------------ended-------------------')})
+
+
+    session.on('connecting', () => {
+        stream = new MediaStream()
+        session.connection.addEventListener('track', (e) => {
+            stream.addTrack(e.track)
+            remoteAudioControl.srcObject = stream
+            remoteAudioControl.play()
+        })
+    })
+    session.on('progress', () => {
+        playSound("ringback.ogg", true)
+    })
+    session.on('accepted', () => {
+        stopSound("ringback.ogg")
+    })
+    session.on('failed', () => {
+        stopSound("ringback.ogg")
+        playSound("rejected.mp3", false)
+        callEnd()
+    })
+    session.on('ended', callEnd)
+}
+
 
 function hangUp() {
-    this.session.terminate();
-    JsSIP.Utils.closeMediaStream(this._localClonedStream);
+    session.terminate()
 }
+
 
 function playSound(soundName, loop) {
-    this._soundsControl.pause();
-    this._soundsControl.currentTime = 0.0;
-    this._soundsControl.src = "sounds/" + soundName
-    this._soundsControl.loop = loop;
-    this._soundsControl.play();
+    soundsControl.pause()
+    soundsControl.currentTime = 0.0
+    soundsControl.src = "sounds/" + soundName
+    soundsControl.loop = loop;
+    soundsControl.play()
 }
 
+
 function stopSound() {
-    this._soundsControl.pause();
-    this._soundsControl.currentTime = 0.0;
+    soundsControl.pause()
+    soundsControl.currentTime = 0.0
 }
